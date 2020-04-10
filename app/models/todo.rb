50d3a6d71df7b61068b71877694cb378bd146cca
karthik51199 <karthik51199@gmail.com>
2020-04-11 00:26:55 +0530

    Using ERB to render todo page

--- app/models/todo.rb
+++ app/models/todo.rb
@@ -3,4 +3,24 @@ class Todo < ActiveRecord::Base
     is_completed = completed ? "[X]" : "[ ]"
     "#{id}. #{due_date.to_s(:long)} #{todo_text} #{is_completed}"
   end
+
+  def due_today?
+    due_date == Date.today
+  end
+
+  def self.overdue
+    where("due_date < ? and completed = ?", Date.today, false)
+  end
+
+  def self.due_today
+    where(due_date: Date.today)
+  end
+
+  def self.due_later
+    where("due_date > ? ", Date.today)
+  end
+
+  def self.completed
+    all.where(completed: true)
+  end
 end
